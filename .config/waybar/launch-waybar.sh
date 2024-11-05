89b02e1fe44650b68b1714d294fff17c849086ae
Jack Mechem <mechemjack@gmail.com>
2024-11-05 14:11:15 -0800

    large commit, will break things.

--- .config/waybar/launch-waybar.sh
+++ .config/waybar/launch-waybar.sh
@@ -5,7 +5,7 @@ CONFIG_FILES="$HOME/.config/waybar/noback/config $HOME/.config/waybar/noback/sty
 trap "killall waybar" EXIT
 
 while true; do
-    waybar -c $HOME/.config/waybar/noback/config -s $HOME/.config/waybar/noback/style.css &
+    waybar -c $HOME/.config/waybar/config -s $HOME/.config/waybar/style.css &
     waybar -c $HOME/.config/waybar/desktopclock/config -s $HOME/.config/waybar/desktopclock/style.css &
     inotifywait -e create,modify $CONFIG_FILES
     killall waybar
